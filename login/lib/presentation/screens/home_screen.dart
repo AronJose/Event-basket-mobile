6992a7ffa9903bd6d80b2c0248a969064a660ecd
aron.jose.tblr <aron.jose.tblr@gmail.com>
2024-08-14 11:32:15 +0530

    clean architeture folder

--- login/lib/presentation/screens/home_screen.dart
+++ login/lib/presentation/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:login/helper/app_bar.dart';
+import 'package:login/helper/bottom_app_bar.dart';
 import 'package:login/presentation/screens/profile_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,51 +11,10 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: const MyAppBarCommon(),
-        bottomNavigationBar: BottomAppBar(
-          height: 60.h,
-          color: const Color.fromARGB(255, 0, 61, 10),
-          shape: const CircularNotchedRectangle(),
-          notchMargin: 2.0,
-          clipBehavior: Clip.antiAlias,
-          child: SizedBox(
-            height: kBottomNavigationBarHeight,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                IconButton(
-                  icon: const Icon(
-                    Icons.home,
-                    color: Colors.white,
-                  ),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.play_circle_outlined,
-                      color: Colors.white),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.favorite_border_outlined,
-                      color: Colors.white),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: const Icon(Icons.account_circle_outlined,
-                      color: Colors.white),
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const ProfileScreen()));
-                  },
-                )
-              ],
-            ),
-          ),
-        ),
+        appBar: MyAppBarCommon(),
+        bottomNavigationBar: BottomAppbarCommon(),
       ),
     );
   }
 }
+
