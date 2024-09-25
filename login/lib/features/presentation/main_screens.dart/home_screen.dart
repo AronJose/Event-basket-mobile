5433b33e9b3a3189ca690cbcfcdacf2b8201f886
aron.jose.tblr <aron.jose.tblr@gmail.co>
2024-09-25 19:54:11 +0530

    home screen issue fix

--- login/lib/features/presentation/main_screens.dart/home_screen.dart
+++ login/lib/features/presentation/main_screens.dart/home_screen.dart
@@ -35,100 +35,102 @@ class HomeScreenState extends State<HomeScreen> {
       padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 2.w),
       child: Card(
         clipBehavior: Clip.antiAlias,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SizedBox(
-              width: imageWidth,
-              height: 200.h,
-              child: Stack(
-                children: [
-                  Image.asset(
-                    images[imgSelectorIndex],
-                    fit: BoxFit.cover,
-                    width: imageWidth,
-                    height: 200.h,
-                  ),
-                  Positioned(
-                    left: 8.0,
-                    top: 90.h,
-                    child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          if (imgSelectorIndex > 0) {
-                            imgSelectorIndex--;
-                          }
-                        });
-                      },
-                      icon: const Icon(Icons.arrow_left),
-                      color: Colors.white,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SizedBox(
+                width: imageWidth,
+                height: 200.h,
+                child: Stack(
+                  children: [
+                    Image.asset(
+                      images[imgSelectorIndex],
+                      fit: BoxFit.cover,
+                      width: imageWidth,
+                      height: 200.h,
                     ),
-                  ),
-                  Positioned(
-                    right: 8.0,
-                    top: 90.h,
-                    child: IconButton(
-                      onPressed: () {
-                        setState(() {
-                          if (imgSelectorIndex < images.length - 1) {
-                            imgSelectorIndex++;
-                          }
-                        });
+                    Positioned(
+                      left: 8.0,
+                      top: 90.h,
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (imgSelectorIndex > 0) {
+                              imgSelectorIndex--;
+                            }
+                          });
+                        },
+                        icon: const Icon(Icons.arrow_left),
+                        color: Colors.white,
+                      ),
+                    ),
+                    Positioned(
+                      right: 8.0,
+                      top: 90.h,
+                      child: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            if (imgSelectorIndex < images.length - 1) {
+                              imgSelectorIndex++;
+                            }
+                          });
+                        },
+                        icon: const Icon(Icons.arrow_right),
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4,
+                        mainAxisSpacing: 2,
+                        crossAxisSpacing: 2,
+                      ),
+                      itemCount: images.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        if (index < images.length) {
+                          return GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                imgSelectorIndex = index;
+                              });
+                            },
+                            child: Card(
+                              clipBehavior: Clip.antiAlias,
+                              child: Image.asset(
+                                images[index],
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          );
+                        }
+                        return null;
                       },
-                      icon: const Icon(Icons.arrow_right),
-                      color: Colors.white,
                     ),
                   ),
                 ],
               ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: GridView.builder(
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 4,
-                      mainAxisSpacing: 2,
-                      crossAxisSpacing: 2,
-                    ),
-                    itemCount: images.length,
-                    shrinkWrap: true,
-                    itemBuilder: (context, index) {
-                      if (index < images.length) {
-                        return GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              imgSelectorIndex = index;
-                            });
-                          },
-                          child: Card(
-                            clipBehavior: Clip.antiAlias,
-                            child: Image.asset(
-                              images[index],
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        );
-                      }
-                      return null;
-                    },
+              Padding(
+                padding: EdgeInsets.all(8.w),
+                child: Text(
+                  "Aron Jose",
+                  style: TextStyle(
+                    fontSize: 30.sp,
+                    fontWeight: FontWeight.w900,
+                    color: const Color(0xFF3A3A3A),
                   ),
                 ),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(8.w),
-              child: Text(
-                "Aron Jose",
-                style: TextStyle(
-                  fontSize: 30.sp,
-                  fontWeight: FontWeight.w900,
-                  color: const Color(0xFF3A3A3A),
-                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
